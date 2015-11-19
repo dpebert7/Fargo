367f047e68eb366fa568b15cb3f3937e5a21ae9a
dpebert7 <dpebert7@gmail.com>
2015-11-19 17:29:37 -0600

    evolution.py updated to include more functionality.

--- evolution.py
+++ evolution.py
@@ -5,8 +5,6 @@ from operator import add
 import functools
 #import matplotlib.pyplot as plt
 
-
-
 """
 Applying genetic algorithm
 Each member of the population should be a (nonincreasing?)
@@ -16,9 +14,31 @@ with 3 dice remaining, the function should continue unless the
 score is ABOVE 800.
 """
 
+def myround(x, base = 50):
+    """
+    round a nunmber to the nearest 50. This function is used in the
+    evolve function to round children to the nearest 50.
+    """
+    return int(base*round(float(x)/base))
+
+
+
+
 def indiv():
-    'create a member of the Fargo population.'
-    return [randint(1,62)*50 for x in range(8)]
+    """
+    create a member of the Fargo population. Each individual
+    should have a reasonabble max and min threshold
+    """
+    vector = []
+    vector.append(randint(9,61)*50) #from 450 to 3050
+    vector.append(randint(8,45)*50) #from 400 to 2250
+    vector.append(randint(7,43)*50) #from 350 to 2150
+    vector.append(randint(6,41)*50) #from 300 to 2050
+    vector.append(randint(5,25)*50) #from 250 to 1250
+    vector.append(randint(4,23)*50) #from 200 to 1150
+    vector.append(randint(3,21)*50) #from 150 to 1050
+    vector.append(randint(2,5)*50)  #from 100 to 250
+    return vector
 
 
 def pop(count):
@@ -30,7 +50,7 @@ def pop(count):
 
 
 conservativeList = [50 for i in range(8)]
-aggressiveList = [50000 for i in range(8)]
+aggressiveList = [5000 for i in range(8)]
 
 def listTurn(stratList, oldScore = 0):
     """
@@ -91,26 +111,30 @@ def gradePop(pop, iterations):
 
 
 
-def evolve(pop, iterations, retain=0.25, add_random=0.02, mutate=0.01):
+def evolve(pop, iterations, retain=0.25, add_random=0.01, mutate=0.02, elite = 0):
     """
     Evolve function for Fargo.
     pop: starting population list
     iterations: number of iterations when testing population members
     retain: fraction of population which will parent the next generation
     add_random: fraction of population to be created randomly
-    mutate: fraction of population that will  mutate 
+    mutate: fraction of population that will  mutate
+    elite: fraction of population from the top that will
+    automatically move on to next generation without mutation
     """
 
     # Sort strongest to weakest
-    graded = [ (fitFun(x, iterations), x) for x in pop]
-    graded = [ x[1] for x in sorted(graded)]
+    graded = [ (fitFun(x, iterations), x) for x in pop ]
+    graded = [ x[1] for x in sorted(graded) ]
     graded.reverse()
-    
+
+    # Determine and save the "elites"
+    elites = elites_length = int(len(graded)*elite)
+    elites = graded[:elites_length]
     
     # Determine parents of next generation
     retain_length = int(len(graded)*retain)
     parents = graded[:retain_length]
-
 
     # Add a few other random individuals to promote genetic diversity
     add_length = int(len(graded)*add_random)
@@ -122,10 +146,12 @@ def evolve(pop, iterations, retain=0.25, add_random=0.02, mutate=0.01):
     for individual in parents:
         if mutate > random():
             pos_to_mutate = randint(0, 7)
-            individual[pos_to_mutate] = randint(1,62)*50
+            tempMutant = indiv()
+            individual[pos_to_mutate] = tempMutant[pos_to_mutate]
 
-
-    # crossover parents to create children
+    
+    # crossover parents to create children using splicing
+    """
     parents_length = len(parents)
     desired_length = len(pop) - parents_length
     children = []
@@ -135,31 +161,50 @@ def evolve(pop, iterations, retain=0.25, add_random=0.02, mutate=0.01):
         if male != female:
             male = parents[male]
             female = parents[female]
-            half = len(male) / 2
-            child = male[:half] + female[half:]
+            switchLocation = randint(1,len(male)-1)
+            child = male[:switchLocation] + female[switchLocation:]
+            children.append(child)
+    """
+
+    # crossover parents to create children using rounded average
+    parents_length = len(parents)
+    desired_length = len(pop) - parents_length
+    children = []
+    while len(children) < desired_length:
+        male = randint(0, parents_length-1)
+        female = randint(0, parents_length-1)
+        if male != female:
+            male = parents[male]
+            female = parents[female]
+            child = [ myround((male[x]+female[x])/2) for x in range(len(male)) ]
             children.append(child)
 
+    
     parents.extend(children)
+    parents[:elites_length] = elites
+    
     return parents
     
 ###################################
 ## IMPORTANT STARTING PARAMATERS ##
 ###################################
 
-iterations = 100
-populationSize = 50
+iterations = 20
+populationSize = 40
 generations = 5
 
-retain=0.25 
-add_random=0.02 
-mutate=0.01
+retain = 0.25
+add_random = 0.02
+mutate = 0.01
+elite = 0.05
 ###################################
+
 
 #Make starting population
 p = pop(populationSize)
 
 #Store fitness history
-fitness_history = [gradePop(p,100),]
+fitness_history = [gradePop(p,iterations),]
 population_history = [p]
 
 #Run evolution
@@ -167,7 +212,7 @@ for i in range(generations):
     p = evolve(p,iterations, retain, add_random, mutate)
     population_history.append(p)
     
-    new_population_grade = gradePop(p,100)
+    new_population_grade = gradePop(p,iterations)
     print (new_population_grade)
     fitness_history.append(new_population_grade)
 
@@ -187,6 +232,6 @@ for individual in fittestPop:
     for i in range(8):
         idealIndiv[i] += individual[i]
     #print(individual)
-idealIndiv = [x/populationSize for x in idealIndiv]
+idealIndiv = [myround(x/populationSize) for x in idealIndiv]
 
 print(idealIndiv)
